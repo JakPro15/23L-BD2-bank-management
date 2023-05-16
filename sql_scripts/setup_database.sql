b70f2c39ff48eebba548e80a3f0029f34f4bd653
Kamil Michalak <01169209@pw.edu.pl>
2023-05-16 17:51:35 +0200

    first table and drop

--- sql_scripts/setup_database.sql
+++ sql_scripts/setup_database.sql
@@ -1,1 +1,12 @@
 USE `bd2-23L-z09`;
+
+CREATE TABLE CLIENTS (
+    client_ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    surname VARCHAR(40) NOT NULL,
+    pesel CHAR(11) UNIQUE,
+    gender CHAR(1) NOT NULL,
+    phone_number VARCHAR(15),
+    email_address VARCHAR(40),
+    CONSTRAINT contact CHECK(phone_number IS NOT NULL OR email_address IS NOT NULL)
+);
