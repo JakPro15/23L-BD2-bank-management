52d4d6119befc7f07132bd10c0829b3156a7cf31
Kamil Michalak <01169209@pw.edu.pl>
2023-05-19 00:09:48 +0200

    added some data creation; fixed inaccuracies in setup

--- sql_scripts/setup_database.sql
+++ sql_scripts/setup_database.sql
@@ -35,7 +35,7 @@ CREATE TABLE KONTA (
     numer_konta CHAR(26) NOT NULL UNIQUE,
     data_utworzenia DATE NOT NULL DEFAULT (CURRENT_DATE),
     data_zakonczenia DATE,
-    limit_transakcji DECIMAL(14, 2),
+    limit_transakcji DECIMAL(40, 20),
     ID_typu_konta INT NOT NULL REFERENCES TYPY_KONTA(ID_typu_konta),
     CONSTRAINT zamkniecie_po_otwarciu CHECK(data_zakonczenia IS NULL OR
                                         data_zakonczenia >= data_utworzenia)
@@ -52,8 +52,8 @@ CREATE TABLE PRZYNALEZNOSCI_KONT (
 CREATE TABLE KARTY (
     ID_karty INT AUTO_INCREMENT PRIMARY KEY,
     numer_karty CHAR(16) NOT NULL UNIQUE,
+    limit_transakcji DECIMAL(40, 20) CHECK(limit_transakcji >= 0),
     data_wygasniecia DATE NOT NULL,
-    limit_transakcji DECIMAL(14, 2) CHECK(limit_transakcji >= 0),
     zablokowana BOOLEAN DEFAULT 0 NOT NULL,
     typ_karty CHAR(6) NOT NULL,
     ID_konta INT NOT NULL REFERENCES KONTA(ID_konta)
@@ -68,14 +68,14 @@ CREATE TABLE WALUTY (
 CREATE TABLE SALDA (
     ID_konta INT NOT NULL REFERENCES KONTA(ID_konta),
     skrot_nazwy_waluty CHAR(3) NOT NULL REFERENCES WALUTY(skrot_nazwy_waluty),
-    obecne_saldo DECIMAL(14, 2) NOT NULL,
+    obecne_saldo DECIMAL(40, 20) NOT NULL,
     CONSTRAINT salda_pk PRIMARY KEY (ID_konta, skrot_nazwy_waluty)
 );
 
 CREATE TABLE POZYCZKI (
     ID_pozyczki INT AUTO_INCREMENT PRIMARY KEY,
-    poczatkowa_kwota DECIMAL(14, 2) NOT NULL CHECK(poczatkowa_kwota > 0),
-    do_splaty DECIMAL(14, 2) NOT NULL CHECK(do_splaty >= 0),
+    poczatkowa_kwota DECIMAL(40, 20) NOT NULL CHECK(poczatkowa_kwota > 0),
+    do_splaty DECIMAL(40, 20) NOT NULL CHECK(do_splaty >= 0),
     data_wziecia DATE DEFAULT (CURRENT_DATE) NOT NULL,
     termin_splaty DATE NOT NULL,
     oprocentowanie DECIMAL(5, 2) NOT NULL CHECK(oprocentowanie >= 0),
@@ -87,7 +87,7 @@ CREATE TABLE POZYCZKI (
 
 CREATE TABLE LOKATY (
     ID_lokaty INT AUTO_INCREMENT PRIMARY KEY,
-    obecna_kwota DECIMAL(14, 2) NOT NULL CHECK(obecna_kwota > 0),
+    obecna_kwota DECIMAL(40, 20) NOT NULL CHECK(obecna_kwota > 0),
     data_zalozenia DATE DEFAULT (CURRENT_DATE) NOT NULL,
     data_zakonczenia DATE,
     data_konca_blokady DATE,
@@ -114,8 +114,8 @@ CREATE TABLE KONTA_ZEWNETRZNE (
 
 CREATE TABLE TRANSAKCJE (
     ID_transakcji INT AUTO_INCREMENT PRIMARY KEY,
-    kwota_przed DECIMAL(14, 2) NOT NULL,
-    kwota_po DECIMAL(14, 2) NOT NULL,
+    kwota_przed DECIMAL(40, 20) NOT NULL,
+    kwota_po DECIMAL(40, 20) NOT NULL,
     data_transakcji DATE DEFAULT (CURRENT_DATE) NOT NULL,
     tytul VARCHAR(50) NOT NULL,
     adres_odbiorcy TEXT,
@@ -124,7 +124,7 @@ CREATE TABLE TRANSAKCJE (
     ID_konta_2 INT REFERENCES SALDA(ID_konta),
     skrot_nazwy_waluty_2 CHAR(3) REFERENCES SALDA(skrot_nazwy_waluty),
     ID_konta_zewnetrznego INT REFERENCES KONTA_ZEWNETRZNE(ID_konta_zewnetrznego),
-    CONSTRAINT łuk CHECK(
+    CONSTRAINT luk CHECK(
         (ID_konta_2 IS NOT NULL AND skrot_nazwy_waluty_2 IS NOT NULL AND ID_konta_zewnetrznego IS NULL)
         OR
         (ID_konta_2 IS NULL AND skrot_nazwy_waluty_2 IS NULL AND ID_konta_zewnetrznego IS NOT NULL)
