649bdea8cf4a97e13e55696e74816bc90b1d6473
Kamil Michalak <01169209@pw.edu.pl>
2023-05-26 17:11:59 +0200

    first trigger for POZYCZKI

--- sql_scripts/procedures_functions.sql
+++ sql_scripts/procedures_functions.sql
@@ -1,1 +1,22 @@
 USE `bd2-23L-z09`;
+
+-- wyzwalacz weryfikuje, czy pozyczka nie jest wzieta na karte debetowa
+-- DROP TRIGGER pozyczka_na_karte_trig;
+delimiter //
+
+CREATE TRIGGER pozyczka_na_karte_trig
+BEFORE INSERT ON POZYCZKI
+FOR EACH ROW
+BEGIN
+    DECLARE v_typ_karty VARCHAR(6);
+
+    IF NEW.ID_karty IS NOT NULL
+    THEN
+        SET @v_typ_karty := (SELECT typ_karty FROM KARTY WHERE ID_karty = NEW.ID_karty);
+
+        IF v_typ_karty = 'debit'
+        THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nie mozna wziac pozyczki na karte debetowa.';
+        END IF;
+    END IF;
+END//
