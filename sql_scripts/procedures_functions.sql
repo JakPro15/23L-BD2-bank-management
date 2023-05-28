436f0377e69925a34340864da0c4f687aab20bba
Kamil Michalak <01169209@pw.edu.pl>
2023-05-28 19:14:27 +0200

    improved loan trigger

--- sql_scripts/procedures_functions.sql
+++ sql_scripts/procedures_functions.sql
@@ -1,25 +1,35 @@
 USE `bd2-23L-z09`;
+DELIMITER //
 
--- wyzwalacz weryfikuje, czy pozyczka nie jest wzieta na karte debetowa
--- DROP TRIGGER pozyczka_na_karte_trig;
-delimiter //
-
-CREATE TRIGGER pozyczka_na_karte_trig
-BEFORE INSERT ON POZYCZKI
-FOR EACH ROW
+-- procedura sprawdza, czy pożyczka nie jest wzięta na kartę debetową
+CREATE PROCEDURE sprawdz_pozyczke_na_karte(IN p_ID_karty INT)
 BEGIN
     DECLARE v_typ_karty VARCHAR(6);
 
-    IF NEW.ID_karty IS NOT NULL
-    THEN
+    IF p_ID_karty IS NOT NULL THEN
         SELECT typ_karty
+        INTO v_typ_karty
         FROM KARTY
-        WHERE ID_karty = NEW.ID_karty
-        INTO v_typ_karty;
+        WHERE ID_karty = p_ID_karty;
 
-        IF v_typ_karty = 'debit'
-        THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nie mozna wziac pozyczki na karte debetowa.';
+        IF v_typ_karty = 'debit' THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nie można wziąć pożyczki na kartę debetową.';
         END IF;
     END IF;
+END//
+
+-- wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed dodaniem rekordu
+CREATE TRIGGER pozyczka_na_karte_before_insert_trig
+BEFORE INSERT ON POZYCZKI
+FOR EACH ROW
+BEGIN
+    CALL sprawdz_pozyczke_na_karte(NEW.ID_karty);
+END//
+
+-- wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed zaktualizowaniem rekordu
+CREATE TRIGGER pozyczka_na_karte_before_update_trig
+BEFORE UPDATE ON POZYCZKI
+FOR EACH ROW
+BEGIN
+    CALL sprawdz_pozyczke_na_karte(NEW.ID_karty);
 END//
--- sql_scripts/testing.sql
+++ sql_scripts/testing.sql
@@ -1,1 +1,9 @@
 USE `bd2-23L-z09`;
+
+-- pozyczka_na_karte_before_insert_trig
+INSERT INTO POZYCZKI VALUES (NULL, 1200, 700, '2021-04-04', '2025-05-05', 25.55, 1, 'PLN', 2);
+-- nie przechodzi, próba wzięcia pożyczki na kartę debetową
+
+-- pozyczka_na_karte_before_update_trig
+UPDATE POZYCZKI SET ID_karty = 2 WHERE ID_pozyczki = 2;
+-- nie przechodzi, próba zaktualizowania pożyczki na kartę debetową
