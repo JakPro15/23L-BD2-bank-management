fd6eba00d23a41f292d3c842cf3222f58f253dae
Kamil Michalak <01169209@pw.edu.pl>
2023-05-28 20:19:09 +0200

    added function calculating total user balance

--- sql_scripts/procedures_functions.sql
+++ sql_scripts/procedures_functions.sql
@@ -1,7 +1,8 @@
 USE `bd2-23L-z09`;
 DELIMITER //
 
--- procedura sprawdza, czy pożyczka nie jest wzięta na kartę debetową
+
+-- Procedura sprawdza, czy pożyczka nie jest wzięta na kartę debetową
 CREATE PROCEDURE sprawdz_pozyczke_na_karte(IN p_ID_karty INT)
 BEGIN
     DECLARE v_typ_karty VARCHAR(6);
@@ -18,7 +19,8 @@ BEGIN
     END IF;
 END//
 
--- wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed dodaniem rekordu
+
+-- Wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed dodaniem rekordu
 CREATE TRIGGER pozyczka_na_karte_before_insert_trig
 BEFORE INSERT ON POZYCZKI
 FOR EACH ROW
@@ -26,10 +28,65 @@ BEGIN
     CALL sprawdz_pozyczke_na_karte(NEW.ID_karty);
 END//
 
--- wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed zaktualizowaniem rekordu
+
+-- Wyzwalacz weryfikuje, czy pożyczka nie jest wzięta na kartę debetową przed zaktualizowaniem rekordu
 CREATE TRIGGER pozyczka_na_karte_before_update_trig
 BEFORE UPDATE ON POZYCZKI
 FOR EACH ROW
 BEGIN
     CALL sprawdz_pozyczke_na_karte(NEW.ID_karty);
 END//
+
+
+-- Funkcja oblicza ogólne saldo w PLN wszystkich kont klienta, wliczając pożyczki i lokaty.
+CREATE FUNCTION policz_calkowite_saldo(p_ID_klienta INT)
+RETURNS DECIMAL
+DETERMINISTIC
+BEGIN
+    DECLARE v_balans_calkowity DECIMAL(40, 20);
+    DECLARE v_suma_lokat DECIMAL(40, 20);
+    DECLARE v_suma_pozyczek DECIMAL(40, 20);
+
+    SELECT SUM(s.obecne_saldo * w.kurs_wymiany_na_PLN)
+    INTO v_balans_calkowity
+    FROM SALDA s
+    INNER JOIN WALUTY w USING(skrot_nazwy_waluty)
+    INNER JOIN KONTA a USING(ID_konta)
+    INNER JOIN PRZYNALEZNOSCI_KONT prk USING(ID_konta)
+    WHERE prk.ID_klienta = p_ID_klienta;
+
+    IF v_balans_calkowity IS NULL
+    THEN
+        SET v_balans_calkowity = 0;
+    END IF;
+
+    SELECT SUM(l.obecna_kwota * w.kurs_wymiany_na_PLN)
+    INTO v_suma_lokat
+    FROM LOKATY l
+    INNER JOIN SALDA s USING(ID_konta, skrot_nazwy_waluty)
+    INNER JOIN WALUTY w USING(skrot_nazwy_waluty)
+    INNER JOIN KONTA k USING(ID_konta)
+    INNER JOIN PRZYNALEZNOSCI_KONT prk USING(ID_konta)
+    WHERE prk.ID_klienta = p_ID_klienta;
+
+    IF v_suma_lokat IS NULL
+    THEN
+        SET v_suma_lokat = 0;
+    END IF;
+
+    SELECT SUM(p.do_splaty * w.kurs_wymiany_na_PLN)
+    INTO v_suma_pozyczek
+    FROM POZYCZKI p
+    INNER JOIN SALDA s USING(ID_konta, skrot_nazwy_waluty)
+    INNER JOIN WALUTY w USING(skrot_nazwy_waluty)
+    INNER JOIN KONTA k USING(ID_konta)
+    INNER JOIN PRZYNALEZNOSCI_KONT prk USING(ID_konta)
+    WHERE prk.ID_klienta = p_ID_klienta;
+
+    IF v_suma_pozyczek IS NULL
+    THEN
+        SET v_suma_pozyczek = 0;
+    END IF;
+
+    RETURN v_balans_calkowity + v_suma_lokat - v_suma_pozyczek;
+END;
--- sql_scripts/testing.sql
+++ sql_scripts/testing.sql
@@ -1,9 +1,37 @@
 USE `bd2-23L-z09`;
 
+
 -- pozyczka_na_karte_before_insert_trig
 INSERT INTO POZYCZKI VALUES (NULL, 1200, 700, '2021-04-04', '2025-05-05', 25.55, 1, 'PLN', 2);
 -- nie przechodzi, próba wzięcia pożyczki na kartę debetową
 
+
 -- pozyczka_na_karte_before_update_trig
 UPDATE POZYCZKI SET ID_karty = 2 WHERE ID_pozyczki = 2;
 -- nie przechodzi, próba zaktualizowania pożyczki na kartę debetową
+
+
+-- Zapytanie demonstruje działanie funkcji policz_calkowite_saldo.
+-- Pokazuje ilość pieniędzy na wszystkich kontach, pożyczkach i lokatach każdego z klientów.
+SELECT ID_klienta, 'konto' as typ, skrot_nazwy_waluty AS waluta, obecne_saldo,
+       policz_calkowite_saldo(ID_klienta) as calkowite_saldo
+FROM KLIENCI
+INNER JOIN PRZYNALEZNOSCI_KONT USING(ID_klienta)
+INNER JOIN KONTA USING(ID_konta)
+INNER JOIN SALDA USING(ID_konta)
+UNION
+SELECT ID_klienta, 'pożyczka' as typ, skrot_nazwy_waluty AS waluta, -do_splaty AS balans,
+       policz_calkowite_saldo(ID_klienta) as calkowite_saldo
+FROM KLIENCI
+INNER JOIN PRZYNALEZNOSCI_KONT USING(ID_klienta)
+INNER JOIN KONTA USING(ID_konta)
+INNER JOIN SALDA USING(ID_konta)
+INNER JOIN POZYCZKI USING(ID_konta, skrot_nazwy_waluty)
+UNION
+SELECT ID_klienta, 'lokata' as typ, skrot_nazwy_waluty AS waluta, obecna_kwota AS balans,
+       policz_calkowite_saldo(ID_klienta) as calkowite_saldo
+FROM KLIENCI
+INNER JOIN PRZYNALEZNOSCI_KONT USING(ID_klienta)
+INNER JOIN KONTA USING(ID_konta)
+INNER JOIN SALDA USING(ID_konta)
+INNER JOIN LOKATY USING(ID_konta, skrot_nazwy_waluty);
