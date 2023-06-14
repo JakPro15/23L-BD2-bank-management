7576c6451ac5e702b047b78f6ddb36a3e478483e
Paweł Kochański <pawilioxion@gmail.com>
2023-06-14 15:33:26 +0200

    formatted testing_sql script output to be more readable

--- src/testing/testing_sql.py
+++ src/testing/testing_sql.py
@@ -1,6 +1,12 @@
 import subprocess
 import pathlib
 import time
+import os
+import re
+
+def print_line():
+    line = "=" * os.get_terminal_size().columns
+    print(f"\033[92m{line}\033[0m\n")
 
 if __name__ == "__main__":
     script_location = pathlib.Path(__file__).parents[2].joinpath("mysql.sh")
@@ -17,58 +23,85 @@ if __name__ == "__main__":
         description = ""
         command = ""
         wait = False
+        count = 0
+
+        line = script.readline()
 
         while True:
-            line = script.readline()
             if not line:
                 break
 
+            while line[:2] == "--":
+                description += line
+                line = script.readline()
+
             if len(line) > 1:
-                while line[:2] == "--":
-                    description += line
+                if count == 0:
+                    print_line()
+
+                print(f"\033[93m{description}\033[0m")
+                description = ""
+
+                while line[-2] != ";":
+                    command += line
                     line = script.readline()
+                command += line.strip()
 
-                if len(line) > 1:
-                    print(description)
-                    description = ""
+                print(f"\033[1m{command}\033[0m\n")
 
-                    while line[-2] != ";":
-                        command += line.strip() + " "
-                        line = script.readline()
-                    command += line.strip()
+                mysql = subprocess.Popen(
+                    [
+                        mysql_location,
+                        "--host=localhost",
+                        f"--socket={pathlib.Path.home().joinpath('bd2_23L_z09_mysql')}/mysqld.sock",
+                        "-u",
+                        "root",
+                        "bd2-23L-z09"
+                    ],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT
+                )
 
-                    print(command + "\n")
-
-                    mysql = subprocess.Popen(
-                        [
-                            mysql_location,
-                            "--host=localhost",
-                            f"--socket={pathlib.Path.home().joinpath('bd2_23L_z09_mysql')}/mysqld.sock",
-                            "-u",
-                            "root",
-                            "bd2-23L-z09"
-                        ],
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT
+                out, _ = mysql.communicate(command.encode())
+                if(len(out) > 0):
+                    result_lines = out.decode().split('\n')[:-1]
+                    result_lines = [re.sub(r"(\.\d*?)0+", lambda matched: matched.group(1), line) for line in result_lines]
+                    result_lines = [re.sub(r"\.([^\d]|$)", lambda matched: matched.group(1), line) for line in result_lines]
+                    result_words = [line.split('\t') for line in result_lines]
+                    max_lens = [0 for _ in result_words[0]]
+                    for line in result_words:
+                        for i, word in enumerate(line):
+                            max_lens[i] = max(max_lens[i], len(word))
+                    result_lines = ["".join([f"{line[i]:<{max_lens[i] + 4}}" for i in range(len(line))]) for line in result_words]
+                                
+                    print(
+                        *["\033[34;1mDBMS OUTPUT:\033[0m " + result_line for result_line in result_lines],
+                        sep='\n'
                     )
+                    print()
 
-                    out, _ = mysql.communicate(command.encode())
-                    print(*["RESULT: " + result_line for result_line in out.decode().split('\n')[:-1]], sep='\n')
+                command = ""
+                wait = True
 
-                    command = ""
-                    wait = True
+                line = script.readline()
 
-                    line = script.readline()
+            while line[:2] == "--":
+                description += line
+                line = script.readline()
 
-                while line[:2] == "--":
-                    description += line
-                    line = script.readline()
+            if len(description) != 0:
+                print(f"\033[93m{description}\033[0m")
+                description = ""
 
-                if len(description) != 0:
-                    print(description)
-                    description = ""
+            count = 0
+            while len(line) == 1:
+                count += 1
+                line = script.readline()
 
-                if wait:
-                    wait = False
+            if wait:
+                wait = False
+                if count == 2:
+                    print_line()
+                    count = 0
                     input()
